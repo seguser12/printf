86f8bdd30a942874ae5fbb7fe72968d6c708a9b9
juliesenynam@gmail.com <juliesenynam@gmail.com>
2022-10-20 10:11:05 -0700

    printf functions

--- functions1.c
+++ functions1.c
@@ -133,7 +133,7 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
-		buffer[i--] = '0'
+		buffer[i--] = '0';
 	buffer[BUFF_SIZE - 1] = '\0';
 
 	while (num > 0)
