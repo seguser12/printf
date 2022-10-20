a4e122a264a26812fcf62cf8bb3f9a7f25b3a77c
juliesenynam@gmail.com <juliesenynam@gmail.com>
2022-10-20 08:01:08 -0700

    more printf functions

--- _printf.c
+++ _printf.c
@@ -1,204 +1,62 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdint.h>
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _withformat4 - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-int _withformat4(char c, int count, va_list valist)
-{
-	uintptr_t p;
-	void *pi;
-
-	switch (c)
-	{
-	case 'p':
-		pi = va_arg(valist, void *);
-		p = (uintptr_t)pi;
-
-		if (pi == NULL)
-		{
-			_printf("(nil)");
-			count += 5;
-		}
-		else
-		{
-			_putchar('0');
-			_putchar('x');
-			count += 2;
-			count += print_hl(p);
-		}
-		break;
-	default:
-		count += 2;
-		_putchar('%');
-		_putchar(c);
-	}
-	return (count);
-}
-
-/**
- * _withformat3 - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-int _withformat3(char c, int count, va_list valist)
-{
-	char *s;
-	int i;
-	char si[6] = "(null)";
-
-	switch (c)
-	{
-	case 'R':
-		s = va_arg(valist, char *);
-		if (!s)
-		{
-			for (i = 0; si[i]; i++, count++)
-				_putchar(si[i]);
-		}
-		else
-			count += rot13(s);
-		break;
-	case 'r':
-		s = va_arg(valist, char *);
-		if (!s)
-		{
-			for (i = 0; si[i]; i++, count++)
-				_putchar(si[i]);
-		}
-		else
-			count += print_rev(s);
-		break;
-		default:
-			count = _withformat4(c, count, valist);
-	}
-	return (count);
-}
-
-/**
- * _withformat2 - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-int _withformat2(char c, int count, va_list valist)
-{
-	unsigned int k;
-
-	switch (c)
-	{
-		case 'b':
-			k = va_arg(valist, unsigned int);
-			count += print_bi(k);
-			break;
-		case 'o':
-			k = va_arg(valist, unsigned int);
-			count += print_octal(k);
-			break;
-		case 'x':
-			k = va_arg(valist, unsigned int);
-			count += print_hexalow(k);
-			break;
-		case 'X':
-			k = va_arg(valist, unsigned int);
-			count += print_hexaup(k);
-			break;
-		case 'u':
-			k = va_arg(valist, unsigned int);
-			count += print_unsig(k);
-			break;
-		default:
-			count = _withformat3(c, count, valist);
-	}
-	return (count);
-}
-
-/**
- * _withformat - prints depending the format
- * @c: the format sent by the main
- * @count: characters printed;
- * @valist: va_list args
- * Return: @count the number of characters printed
- */
-int _withformat(char c, int count, va_list valist)
-{
-	int j, i;
-	char *s;
-	char si[6] = "(null)";
-
-	switch (c)
-	{
-		case 'c':
-			j = va_arg(valist, int);
-			count += _putchar(j);
-			break;
-		case 's':
-			s = va_arg(valist, char *);
-			if (!s)
-			{
-				for (i = 0; si[i]; i++, count++)
-					_putchar(si[i]);
-			}
-			else
-				count += _printstring(s);
-			break;
-		case '%':
-			count += _putchar('%');
-			break;
-		case 'i':
-		case 'd':
-			j = va_arg(valist, int);
-
-			if (!j)
-			{
-				count++;
-				_putchar('0');
-			} else
-				count += print_number(j);
-			break;
-		default:
-			count = _withformat2(c, count, valist);
-	}
-	return (count);
-}
-
-/**
- * _printf - Fuction that prints to the std output
- * @format: list of parameters passed
- * Return: @count the number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	va_list valist;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(valist, format);
-	if (!format)
+	if (format == NULL)
 		return (-1);
-	for (i = 0; format[i]; i++)
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			count++;
-			_putchar(format[i]);
-		}
-		else if (format[i + 1])
-		{
-			i++;
-			count = _withformat(format[i], count, valist);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
-			return (-1);
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
-	va_end(valist);
-	return (count);
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
